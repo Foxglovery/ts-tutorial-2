ae13133d8f229617fbe4889a5c2bce865647d2b5
Foxglovery <foxglovery@gmail.com>
2024-10-07 12:09:03 -0500

    Classes and constructors

--- build/js/main.js
+++ build/js/main.js
@@ -1,27 +1,67 @@
 "use strict";
-// convert to more or less specific
-let a = 'hello';
-let b = a; // assigned to less specific type
-let c = a; // more specific
-// using angle brackets
-let d = 'world';
-let e = 'world';
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b;
-};
-// have told ts the function returns a string in this instance
-let myVal = addOrConcat(2, 2, 'concat');
-// be careful! TS wont see a problem here but a string is returned
-let nextVal = addOrConcat(2, 2, 'concat');
-// Double Casting or Force Casting
-//10 as string // will become below
-10;
-// The DOM
-// const img = document.getElementById('img') as HTMLImageElement // non null assertion
-// const myImg = document.getElementById('#img') as HTMLImageElement
-// // bracket notation
-// const nextImg = <HTMLImageElement>document.getElementById('#img')
-// img.src
-// myImg.src
+// CLASSES
+// properties need to be declared and initialized with a constructor
+// can be kept dry by using visibility modifiers: public, readonly etc
+// will need to remove the declaration
+class Coder {
+    constructor(name, music, 
+    // private property only accessible inside of the class using methods from the class like getAge()
+    age, 
+    // protected is only accessible inside the class and its subclasses
+    lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        // dont technically need these anymore
+        this.name = name;
+        this.music = music;
+        this.age = age,
+            this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I am ${this.age}`;
+    }
+}
+const Gabe = new Coder('Gabe', 'Scree', 36);
+console.log(Gabe.getAge());
+// EXTEND A CLASS
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        // call to super is required when extending, include what you are passing from the other class
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    // added method to get lang since this is a subclass
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25);
+console.log(Sara.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+//////////////////////////////////////////////////////////
+// STATIC : does not refer to any instantiation of the class, but the class directly
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        // putting the ++ on left makes count increment first
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,98 @@
-type One = string
-type Two = string | number
-type Three = 'hello'
+// CLASSES
 
-// convert to more or less specific
-let a: One = 'hello'
-let b = a as Two // assigned to less specific type
-let c = a as Three // more specific
-// using angle brackets
-let d = <One>'world'
-let e = <Two>'world'
+// properties need to be declared and initialized with a constructor
+// can be kept dry by using visibility modifiers: public, readonly etc
+// will need to remove the declaration
+class Coder {
 
-const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
-    if (c === 'add') return a + b
-    return '' + a + b
+    // unusual example for adding property that wont be initialized right away
+    secondLang!: string
+
+    constructor(
+        public readonly name: string,
+        public music: string,
+        // private property only accessible inside of the class using methods from the class like getAge()
+        private age: number,
+        // protected is only accessible inside the class and its subclasses
+        protected lang: string = 'Typescript'
+    ) {
+        // dont technically need these anymore
+        this.name = name
+        this.music = music
+        this.age = age,
+            this.lang = lang
+    }
+
+    public getAge() {
+        return `Hello, I am ${this.age}`
+    }
 }
-// have told ts the function returns a string in this instance
-let myVal: string = addOrConcat(2, 2, 'concat') as string
-// be careful! TS wont see a problem here but a string is returned
-let nextVal: number = addOrConcat(2, 2, 'concat') as number
 
-// Double Casting or Force Casting
-//10 as string // will become below
-(10 as unknown) as string
+const Gabe = new Coder('Gabe', 'Scree', 36)
+console.log(Gabe.getAge())
 
-// The DOM
-// const img = document.getElementById('img') as HTMLImageElement // non null assertion
-// const myImg = document.getElementById('#img') as HTMLImageElement
-// // bracket notation
-// const nextImg = <HTMLImageElement>document.getElementById('#img')
+// EXTEND A CLASS
 
+class WebDev extends Coder {
+    constructor(
+        public computer: string,
+        name: string,
+        music: string,
+        age: number
+    ) {
+        // call to super is required when extending, include what you are passing from the other class
+        super(name, music, age)
+        this.computer = computer
+    }
+    // added method to get lang since this is a subclass
+    public getLang() {
+        return `I write ${this.lang}`
+    }
+}
 
-// img.src
-// myImg.src
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25)
 
+console.log(Sara.getLang())
 
+///////////////////////////////////////////////////
+
+// Applying interface to class
+
+interface Musician {
+    name: string,
+    instrument: string,
+    play(action: string): string
+}
+class Guitarist implements Musician {
+    name: string
+    instrument: string
+
+    constructor(name: string, instrument: string) {
+        this.name = name
+        this.instrument = instrument
+    }
+
+    play(action: string) {
+        return `${this.name} ${action} the ${this.instrument}`
+    }
+}
+
+const Page = new Guitarist('Jimmy', 'guitar')
+console.log(Page.play('strums'))
+//////////////////////////////////////////////////////////
+// STATIC : does not refer to any instantiation of the class, but the class directly
+class Peeps {
+    static count: number = 0
+
+    static getCount(): number {
+        return Peeps.count
+    }
+
+    public id: number
+
+    constructor(public name: string) {
+        this.name = name
+        // putting the ++ on left makes count increment first
+        this.id = ++Peeps.count
+    }
+}
